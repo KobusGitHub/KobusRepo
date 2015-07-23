6f0d1d0ff28e9568022c7d7357079103eed0047a
Kobus <kjonker@cyestcorp.com>
2015-07-23 09:56:56 +0200

    Commit

--- AngularProject/AngularProject/Scripts/App.js
+++ AngularProject/AngularProject/Scripts/App.js
@@ -1,8 +1,10 @@
-﻿var appMainModule = angular.module('appMain', []);
+﻿var appMainModule = angular.module('appMain', ['ui.bootstrap', 'ui.bootstrap.dropdown']);
+
+
 
 appMainModule.controller("homePageViewModel", function ($scope, $http, $location)
 {
-    $scope.headingCaption = 'My Headiing caption';
+    $scope.headingCaption = 'My Heading caption';
 
     $scope.people = [
         { FirstName: "Kobus", LastName: "Jonker" },
@@ -12,5 +14,36 @@ appMainModule.controller("homePageViewModel", function ($scope, $http, $location
 
     $scope.showPerson = function(person) {
         alert('You selected ' + person.FirstName + ' ' + person.LastName);
+        $scope.selectedPerson = person;
+       
+        var newPerson = { FirstName: "Kobus", LastName: "Jonker" }
+
+        $scope.people.push(newPerson);
+        
+        
+        
+
     }
+
+    $scope.selectedPerson = undefined;
+
+    $scope.sendInvite = function (person) {
+        alert('You selected ' + person.FirstName + ' ' + person.LastName);
+    }
+
+
+
+
+    $scope.items = [
+    'Option 1 from viewModel',
+    'Option 2 from viewModel',
+    'Option 3 from viewModel'
+    ];
+
+
+    $scope.dropdownText = '--SELECT--';
+    $scope.dropdownSelected = function (choice) {
+        $scope.dropdownText = choice;
+    }
+
 })
